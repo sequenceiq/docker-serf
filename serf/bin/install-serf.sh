1a155b41cbb953ea957a05e93dd52ac4354d5391
lalyos <lalyos@yahoo.com>
2014-05-20 07:08:23 +0200

    update serf v0.6.0

--- serf/bin/install-serf.sh
+++ serf/bin/install-serf.sh
@@ -13,7 +13,7 @@ TEMP_ZIP=/tmp/serf.zip
 SERF_HOME=/usr/local/serf
 SERF_BIN=$SERF_HOME/bin/serf
 
-curl -Lso $TEMP_ZIP https://dl.bintray.com/mitchellh/serf/0.5.0_linux_amd64.zip
+curl -Lso $TEMP_ZIP https://dl.bintray.com/mitchellh/serf/0.6.0_linux_amd64.zip
 unzip $TEMP_ZIP -d $(dirname $SERF_BIN)
 ln -s $SERF_BIN /usr/local/bin/serf
 rm $TEMP_ZIP
