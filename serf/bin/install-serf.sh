e95be11a96ca8f299e6319e5e1067ae8da5588be
lalyos <lalyos@yahoo.com>
2014-06-11 06:37:45 +0200

    one-liner installer added for shot ref for Dockerfiles

--- serf/bin/install-serf.sh
+++ serf/bin/install-serf.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+############ itstarted out from the oneliner:
+# yum install -y unzip curl && curl -Lso /tmp/serf.zip https://dl.bintray.com/mitchellh/serf/0.6.0_linux_amd64.zip && mkdir -p /usr/local/serf/bin && unzip /tmp/serf.zip -d /usr/local/serf/bin && ln -s /usr/local/serf/bin/serf /usr/local/bin/serf && rm /tmp/serf.zip
+############
+
 command_exists() {
 	command -v "$@" > /dev/null 2>&1
 }
